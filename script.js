4af598956000942bee3ef04fb51bfe460046bbae
adminti25 <david.ugalde@hsai.com.mx>
2026-06-04 16:29:14 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json"
-          "ngrok-skip-browser-warning": "true"
         },
         body: JSON.stringify(datosMembresia)
       });
