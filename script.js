92df3d55f9cd038b07cc0af6088f4a7394803722
adminti25 <david.ugalde@hsai.com.mx>
2026-06-04 15:55:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json"
+          "ngrok-skip-browser-warning": "true"
         },
         body: JSON.stringify(datosMembresia)
       });
